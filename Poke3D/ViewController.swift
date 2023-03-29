9786c5b902698ae5b007a6a10e65103a42c16a12
Suren <105368039+wwwsurenio@users.noreply.github.com>
2023-03-29 23:27:42 +0200

    initial setup for an AR app that uses image tracking

--- Poke3D/ViewController.swift
+++ Poke3D/ViewController.swift
@@ -22,18 +22,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = ARImageTrackingConfiguration()
+        
+        if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: "PokemonCards", bundle: Bundle.main){
+            
+            configuration.trackingImages = imageToTrack
+            configuration.maximumNumberOfTrackedImages = 1
+            
+            print("Images succesfully added")
+            
+        }
+        
+        
 
         // Run the view's session
         sceneView.session.run(configuration)
